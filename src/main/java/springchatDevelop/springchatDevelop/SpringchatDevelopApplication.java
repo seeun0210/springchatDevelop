2c2e6df14f5ef762e9c4633da87fee3e682c579c
kimseeun <cocobell3@naver.com>
2023-12-09 11:18:20 +0900

    Initial commit

---
+++ src/main/java/springchatDevelop/springchatDevelop/SpringchatDevelopApplication.java
@@ -0,0 +1,13 @@
+package springchatDevelop.springchatDevelop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringchatDevelopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringchatDevelopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springchatDevelop/springchatDevelop/SpringchatDevelopApplicationTests.java
@@ -0,0 +1,13 @@
+package springchatDevelop.springchatDevelop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringchatDevelopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
